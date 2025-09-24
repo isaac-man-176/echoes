4d06e092fcde9c29c50577a5f4d0c8edb8490ba3
Isaac Man <103552680+haydayisawsome@users.noreply.github.com>
2025-09-24 03:39:23 -0400

    fixed api link from local to deployed version

--- src/api.js
+++ src/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "http://localhost:5000/api", // your backend URL
+  baseURL: "https://echoes-ed6b.onrender.com/", // your backend URL
 });
 
 export default API;
